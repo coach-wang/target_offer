2fdee333d6098870edf94c16b7c540f1e392ad18
coach-wang <51986254+coach-wang@users.noreply.github.com>
2020-03-07 21:16:19 +0800

    Update test06_从尾到头打印链表.py

--- test06_从尾到头打印链表.py
+++ test06_从尾到头打印链表.py
@@ -1,7 +1,7 @@
 class ListNode:
-     def __init__(self, x):
-         self.val = x
-         self.next = None
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
     # 返回从尾部到头部的列表值序列，例如[1,2,3]
