58239020db6c76031ae228fd19326523f635c4d0
coach-wang <51986254+coach-wang@users.noreply.github.com>
2020-03-07 21:45:46 +0800

    Create test15_二进制中1的个数.py

---
+++ test15_二进制中1的个数.py
@@ -0,0 +1,25 @@
+#1 左移法
+class Solution:
+    def NumberOf1(self, n):
+        count, flag = 0, 1
+        if n < 0:
+            n = n & 0xffffffff
+            #n *= -1
+            #count = 1 这种方法遇到-214783648时取正数时会溢出，不可取
+        while flag and flag <=n:
+            if n & flag:
+                count += 1
+            flag = flag << 1
+        return count
+        
+#减1做与运算
+class Solution:
+    def NumberOf1(self, n):
+        count = 0
+        if n < 0:
+            n = n & 0xffffffff
+        while n:
+            count += 1
+            n = (n-1) & n
+        return count
+    
