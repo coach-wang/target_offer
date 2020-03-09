8a94ee24f1260c48dbb41173e0593d188961bc0d
coach-wang <51986254+coach-wang@users.noreply.github.com>
2020-03-09 14:55:49 +0800

    Create test41_数据流中的中位数.py

---
+++ test41_数据流中的中位数.py
@@ -0,0 +1,14 @@
+class Solution:
+    def __init__(self):
+        self.data=[]
+    def Insert(self, num):
+        # write code here
+        self.data.append(num)
+        self.data.sort()
+    def GetMedian(self,data):
+        # write code here
+        length=len(self.data)
+        if length%2==0:
+            return (self.data[length//2]+self.data[length//2-1])/2.0
+        else:
+            return self.data[length//2]
